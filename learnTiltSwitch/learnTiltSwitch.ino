cf7aa9bdf494c0fac8f3eccc7e7222f2b91cefcf
AniTx10 <139654611+AniTx10@users.noreply.github.com>
2025-07-15 19:03:35 -0500

    Create learnTiltSwitch.ino

---
+++ learnTiltSwitch/learnTiltSwitch.ino
@@ -0,0 +1,36 @@
+const int tiltPin = 2;
+const int greenPin = 9;
+const int redPin = 10;
+int tiltVal;
+
+int dt = 50;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(tiltPin, INPUT_PULLUP);
+  pinMode(greenPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  tiltVal = digitalRead(tiltPin);
+  Serial.println(tiltVal);
+
+  if(tiltVal == 0){
+    digitalWrite(redPin, HIGH);
+    digitalWrite(greenPin, LOW);
+  }
+
+  else{
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+  }
+
+  delay(dt);
+
+}
